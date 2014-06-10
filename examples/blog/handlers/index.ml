e40a8a0d5f37385a1762b436d01daf5e1259b3bc
Phil <peaton@haverford.edu>
2014-06-09 23:46:57 -0400

    Added some more examples. Todo: add include templates

--- examples/blog/handlers/index.ml
+++ examples/blog/handlers/index.ml
@@ -1,2 +1,4 @@
+let file = "examples/blog/templates/index.html"
+
 let handler request_uri request_method =
-	let template = Utils.read_file_to_string "templates/index.html" in template
+	let template = Utils.read_file_to_string file in template
